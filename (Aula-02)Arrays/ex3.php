b023ef083b0831e5836723cfacaa953f9e0be19d
Gabriel Santos <134400952+GabzinhoHD@users.noreply.github.com>
2025-08-13 21:01:27 -0300

    Update ex3.php

--- (Aula-02)Arrays/ex3.php
+++ (Aula-02)Arrays/ex3.php
@@ -7,5 +7,11 @@ $turisticos = array("Itaipu", "Cataratas", "Parque das aves");
 
 $matriz = array($flores, $frutas, $cidades, $turisticos);
 
-
+echo "<table><br>";
+foreach($matriz as $item){
+    echo "<tr><br>";
+    echo "<td>" . $item[0] ."</td>" . "<td>" . $item[1] . "</td>" ."<td>" . $item[2] . "</td>";
+    echo "</tr><br>";
+}
+echo "</table>"
 ?>
