b460d13e996502eb3ef75a2b4a6910866392502e
Gabriel Santos <134400952+GabzinhoHD@users.noreply.github.com>
2025-08-19 19:30:01 -0300

    Add files via upload

--- (Aula-04)Get-Post/ex3.php
+++ (Aula-04)Get-Post/ex3.php
@@ -1,4 +1,20 @@
 <?php
+echo "EXIBINDO NUMEROS:<br>";
 
+$numIni = 1;
+$numFim = 100;
+
+if(isset($_GET["inicio"]) && $_GET["inicio"] >= 1) $numIni = $_GET["inicio"];
+if(isset($_GET["fim"]) && $_GET["fim"] <= 100) $numFim = $_GET["fim"];
+
+if($numIni > $numFim){
+    echo "Numeros inicial maior que o numero final!!";
+}
+
+else{
+    for($i = $numIni; $i <= $numFim; $i++){
+        echo $i . "<br>";
+    }
+}
 
 ?>
