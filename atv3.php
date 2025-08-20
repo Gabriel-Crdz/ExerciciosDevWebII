bb1486531a68229609d8875bac2bab5aa8103aa3
Gabriel Santos <134400952+GabzinhoHD@users.noreply.github.com>
2025-08-19 21:15:28 -0300

    Create atv3.php

---
+++ atv3.php
@@ -0,0 +1,34 @@
+
+<?php
+function exibir($valid){
+    if($valid == True){
+        echo '
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            <form action="" method="post">
+                <input type="text" name="login" placeholder="login">
+                <input type="text" name="senha" placeholder="senha">
+                <br>
+                <button>Enviar<button>
+            </form>
+        </body>
+        </html>';
+    }
+}
+
+$login = $_POST["login"];
+$senha = $_POST["senha"];
+exibir(True);
+
+if($login == "ifpr" && $senha == "tads"){
+    exibir(False);
+    echo "Bem-vindo ao TADS!!";
+}
+
+?>
